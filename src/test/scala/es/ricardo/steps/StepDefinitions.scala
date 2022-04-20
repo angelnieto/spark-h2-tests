1cb7a0ab5952f4f7a8ca94886d82bd9a396fc387
WP99CQ <WP99CQ@WPU8L0087595.ad.ing.net>
2022-04-20 09:16:59 +0200

    Changed step When

--- src/test/scala/es/ricardo/steps/StepDefinitions.scala
+++ src/test/scala/es/ricardo/steps/StepDefinitions.scala
@@ -3,7 +3,7 @@ package es.ricardo.steps
 import io.cucumber.scala.{EN, ScalaDsl}
 import org.junit.AfterClass
 import org.junit.BeforeClass
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, DataFrame}
 import es.ricardo.database.{H2RowModifier, H2DatabaseCreator}
 import es.ricardo.model.RowModification
 import es.ricardo.App
@@ -18,6 +18,8 @@ class StepDefinitions extends ScalaDsl with EN {
   
   implicit val spark = SparkSession.builder.appName("test").master("local").getOrCreate
   import spark.implicits._
+  
+  var students: DataFrame = _
   
   Given("""an {string} operation in table {string} to insert a new student with the value {string} in the field {string} and the value {string} in the field {string}""") { 
     (operation: String, table: String, value1: String, field1: String, value2: String, field2: String) =>
@@ -41,12 +43,12 @@ class StepDefinitions extends ScalaDsl with EN {
      new H2RowModifier().saveOrUpdate(H2DatabaseCreator.outputDbConf, rowModification)
   }
   
-  When("""the student data is collected""") { () =>
+  When("""the {string} data is collected""") { (table: String) =>
+    students = new App().readTable(table)
   }
   
   Then("""the student in the table {string} with field {string} with value {int} changed the field {string} to the value {string}""") {
     (table: String, wField1: String, wValue1: Int, field1: String, value1: String) =>
-    val students = new App().readTable(table)
     println("students")
     students.show()
     Assert.assertEquals(value1, students.filter(col(wField1).===(wValue1)).first().getAs(field1.toUpperCase()))

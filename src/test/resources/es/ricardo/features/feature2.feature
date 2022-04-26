14e33d5682b6e80d791d6a3be25d57edd7440e00
WP99CQ <WP99CQ@WPU8L0087595.ad.ing.net>
2022-04-26 17:14:48 +0200

    Tests running

--- src/test/resources/es/ricardo/features/feature2.feature
+++ src/test/resources/es/ricardo/features/feature2.feature
@@ -4,6 +4,6 @@ Description: The purpose of this feature is to test the modification and inserti
  
 Scenario: Modification and creation of lawyers
     Given an "insert" operation in table "ally_macbeal.lawyer" to insert a new lawyer with the value "Nelle" in the field "first_name" and the value "Porter" in the field "last_name"
-    And an "update" operation in table "ally_macbeal.lawyer" we set the value "Ally" in the field "first_name" for the student with field "id" with value 1
+    And an "update" operation in table "ally_macbeal.lawyer" we set the value "Ally" in the field "first_name" for the lawyer with field "id" with value 1
     When the "ally_macbeal.lawyer" data is collected 
-    Then the student in the table "ally_macbeal.lawyer" with field "id" with value 1 changed the field "first_name" to the value "Ally" 
+    Then the lawyer in the table "ally_macbeal.lawyer" with field "id" with value 1 changed the field "first_name" to the value "Ally" 
--- src/test/scala/es/ricardo/steps/StepDefinitions.scala
+++ src/test/scala/es/ricardo/steps/StepDefinitions.scala
@@ -20,7 +20,7 @@ class StepDefinitions extends ScalaDsl with EN {
   
   var lawyers: DataFrame = _
   
-  Given("""an {string} operation in table {string} to insert a new student with the value {string} in the field {string} and the value {string} in the field {string}""") { 
+  Given("""an {string} operation in table {string} to insert a new lawyer with the value {string} in the field {string} and the value {string} in the field {string}""") { 
     (operation: String, table: String, value1: String, field1: String, value2: String, field2: String) =>
      var fields = Map[String, Any]() 
      fields += (field1 -> value1)
@@ -31,7 +31,7 @@ class StepDefinitions extends ScalaDsl with EN {
      
   }
   
-  And("""an {string} operation in table {string} we set the value {string} in the field {string} for the student with field {string} with value {int}""") { 
+  And("""an {string} operation in table {string} we set the value {string} in the field {string} for the lawyer with field {string} with value {int}""") { 
     (operation: String, table: String, value1: String, field1: String, wField: String, wValue: Int) =>
      var fields = Map[String, Any]() 
      fields += (field1 -> value1)
@@ -48,12 +48,12 @@ class StepDefinitions extends ScalaDsl with EN {
     lawyers.show()
   }
   
-  Then("""the student in the table {string} with field {string} with value {int} changed the field {string} to the value {string}""") {
+  Then("""the lawyer in the table {string} with field {string} with value {int} changed the field {string} to the value {string}""") {
     (table: String, wField1: String, wValue1: Int, field1: String, value1: String) =>
     Assert.assertEquals(value1, lawyers.filter(col(wField1).===(wValue1)).first().getAs(field1.toUpperCase()))
   }
   
-  Then("""the student in the table {string} with field {string} with value {string} has the value {string} in the field {string}""") { 
+  Then("""the lawyer in the table {string} with field {string} with value {string} has the value {string} in the field {string}""") { 
     (table: String, field1: String, value1: String, value2: String, field2: String) =>
     Assert.assertEquals(value2, lawyers.filter(col(field1).===(value1)).first().getAs(field2.toUpperCase()))
   }
